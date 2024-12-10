95e889261bc72e8d279c3079592acf025d79b8f4
Muhammad Ibrahim <ibrahim.malik.9634@gmail.com>
2024-12-10 10:48:23 +0500

    coordinates/zoom stored

--- dummy-dashboard/src/App.jsx
+++ dummy-dashboard/src/App.jsx
@@ -1,20 +1,39 @@
-import { useRef, useEffect } from 'react'
+import { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-
 import './App.css'
 
-function App() {
+const INITIAL_CENTER = [
+  74.30091848275428,
+  31.479157820992256
+]
+const INITIAL_ZOOM = 10.12
 
+function App() {
   const mapRef = useRef()
   const mapContainerRef = useRef()
+
+  const [center, setCenter] = useState(INITIAL_CENTER)
+  const [zoom, setZoom] = useState(INITIAL_ZOOM)
 
   useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiaWJyYWhpbW1hbGlrMjAwMiIsImEiOiJjbTQ4OGFsZ2YwZXIyMmlvYWI5a2lqcmRmIn0.rBsosB8v7n08Vkq1UHH_Pw'
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      center: center,
+      zoom: zoom
     });
+
+    mapRef.current.on('move', () => {
+      // get the current center coordinates and zoom level from the map
+      const mapCenter = mapRef.current.getCenter()
+      const mapZoom = mapRef.current.getZoom()
+
+      // update state
+      setCenter([ mapCenter.lng, mapCenter.lat ])
+      setZoom(mapZoom)
+    })
 
     return () => {
       mapRef.current.remove()
@@ -23,7 +42,10 @@ function App() {
 
   return (
     <>
-      <div id='map-container' ref={mapContainerRef}/>
+      <div className="sidebar">
+        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+      </div>
+      <div id='map-container' ref={mapContainerRef} />
     </>
   )
 }
