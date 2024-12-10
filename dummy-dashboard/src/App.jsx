ca9feed9592e6ae5c182b118d1fef6440a06686f
Muhammad Ibrahim <ibrahim.malik.9634@gmail.com>
2024-12-10 11:20:34 +0500

    reset map working

--- dummy-dashboard/src/App.jsx
+++ dummy-dashboard/src/App.jsx
@@ -1,31 +1,39 @@
-import { useRef, useEffect, useState } from 'react'
-import mapboxgl from 'mapbox-gl'
+import { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-import './App.css'
+import './App.css';
 
-const INITIAL_CENTER = [
-  74.30091848275428,
-  31.479157820992256
-]
-const INITIAL_ZOOM = 10.12
+const INITIAL_CENTER = [74.30091848275428, 31.479157820992256];
+const INITIAL_ZOOM = 10.12;
 
 function App() {
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
+  const mapRef = useRef();
+  const mapContainerRef = useRef();
 
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
+  const [center, setCenter] = useState(INITIAL_CENTER);
+  const [zoom, setZoom] = useState(INITIAL_ZOOM);
 
   useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiaWJyYWhpbW1hbGlrMjAwMiIsImEiOiJjbTQ4OGFsZ2YwZXIyMmlvYWI5a2lqcmRmIn0.rBsosB8v7n08Vkq1UHH_Pw'
+    mapboxgl.accessToken =
+      'pk.eyJ1IjoiaWJyYWhpbW1hbGlrMjAwMiIsImEiOiJjbTQ4OGFsZ2YwZXIyMmlvYWI5a2lqcmRmIn0.rBsosB8v7n08Vkq1UHH_Pw';
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: center,
       zoom: zoom,
     });
-  
+
+    mapRef.current = map;
+
+    map.on('move', () => {
+      const mapCenter = map.getCenter();
+      const mapZoom = map.getZoom();
+
+      setCenter([mapCenter.lng, mapCenter.lat]);
+      setZoom(mapZoom);
+    });
+
     map.on('load', () => {
       // Add GeoJSON Source
       map.addSource('custom-layer', {
@@ -54,11 +62,11 @@ function App() {
                 title: 'Cholistan Office',
                 description: 'This is the project office of Nespak.',
               },
-            }
+            },
           ],
         },
       });
-  
+
       // Add a Layer using the source
       map.addLayer({
         id: 'custom-layer',
@@ -69,47 +77,51 @@ function App() {
           'circle-color': '#007cbf',
         },
       });
-  
+
       // Add Popup to Marker
       map.on('click', 'custom-layer', (e) => {
         const coordinates = e.features[0].geometry.coordinates.slice();
         const { title, description } = e.features[0].properties;
-  
+
         new mapboxgl.Popup()
           .setLngLat(coordinates)
           .setHTML(`<h3>${title}</h3><p>${description}</p>`)
           .addTo(map);
       });
-  
+
       // Change cursor to pointer when hovering over the layer
       map.on('mouseenter', 'custom-layer', () => {
         map.getCanvas().style.cursor = 'pointer';
       });
-  
+
       map.on('mouseleave', 'custom-layer', () => {
         map.getCanvas().style.cursor = '';
       });
     });
-  
+
     return () => map.remove();
   }, []);
 
   const handleButtonClick = () => {
-    mapRef.current.flyTo({
-      center: INITIAL_CENTER,
-      zoom: INITIAL_ZOOM
-    })
-  }
+    if (mapRef.current) {
+      mapRef.current.flyTo({
+        center: INITIAL_CENTER,
+        zoom: INITIAL_ZOOM,
+      });
+    }
+  };
 
   return (
     <>
       <div className="sidebar">
         Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
       </div>
-      <button onClick={handleButtonClick} className='reset-button'>Reset</button>
-      <div id='map-container' ref={mapContainerRef} />
+      <button onClick={handleButtonClick} className="reset-button">
+        Reset
+      </button>
+      <div id="map-container" ref={mapContainerRef} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
