d41685d677bd467f499fb60da9dc0bf3aa7cf54c
Muhammad Ibrahim <ibrahim.malik.9634@gmail.com>
2024-12-16 11:28:26 +0500

    .

--- dummy-dashboard/src/components/Buttons.jsx
+++ dummy-dashboard/src/components/Buttons.jsx
@@ -3,33 +3,35 @@ import '../styles/App.css';
 
 function Buttons({ onReset, onBasemapChange, onRemoveLayer }) {
   return (
-    <div className="buttons-container">
-      <button
-        className="basemap-button"
-        onClick={() => onBasemapChange('mapbox://styles/mapbox/streets-v11')}
-      >
-        Streets
-      </button>
-      <button
-        className="basemap-button"
-        onClick={() => onBasemapChange('mapbox://styles/mapbox/satellite-v9')}
-      >
-        Satellite
-      </button>
-      <button
-        className="basemap-button"
-        onClick={() =>
-          onBasemapChange('mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y')
-        }
-      >
-        3D Map
-      </button>
-      {/* <button className="remove-button" onClick={onRemoveLayer}>
-        Remove Shapefile
-      </button> */}
-      <button onClick={onReset} className="reset-button">
-        Reset
-      </button>
+    <div className="buttons-wrapper">
+      <div className="buttons-container">
+        <button
+          className="basemap-button"
+          onClick={() => onBasemapChange('mapbox://styles/mapbox/streets-v11')}
+        >
+          Streets
+        </button>
+        <button
+          className="basemap-button"
+          onClick={() => onBasemapChange('mapbox://styles/mapbox/satellite-v9')}
+        >
+          Satellite
+        </button>
+        <button
+          className="basemap-button"
+          onClick={() =>
+            onBasemapChange('mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y')
+          }
+        >
+          3D Map
+        </button>
+        {/* <button className="remove-button" onClick={onRemoveLayer}>
+          Remove Shapefile
+        </button> */}
+        <button onClick={onReset} className="reset-button">
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
